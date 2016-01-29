c3c4e703aac7731511862b83be00aa3b06e9caf5
kartojal <canillas.mail@gmail.com>
2016-01-29 16:42:16 +0100

    Update checkmilestones.py
    Added tabulation to description

--- checkmilestones.py
+++ checkmilestones.py
@@ -5,10 +5,10 @@
 #   beautifulsoup4 (You can install it throught pip )
 #   
 # Description: 
-# Python script to alarm you with N beeps and an alert window when any milestone reach the
-# desired percentage at Github. Only for Windows for now. Combine with cron/"Schedule Tasks" 
-# or set "while True: + sleep" after Main comentary if you are lazy, to repeat the script as your needs. 
-# Be the first to know the big news!
+#   Python script to alarm you with N beeps and an alert window when any milestone reach the
+#   desired percentage at Github. Only for Windows for now. Combine with cron/"Schedule Tasks" 
+#   or set "while True: + sleep" after Main comentary if you are lazy, to repeat the script as your needs. 
+#   Be the first to know the big news!
 
 from bs4 import BeautifulSoup
 from time import sleep
