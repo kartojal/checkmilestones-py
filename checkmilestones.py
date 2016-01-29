89d97a40227373a0d5c37528dc4f37824b287bdb
kartojal <canillas.mail@gmail.com>
2016-01-29 16:34:50 +0100

    Update checkmilestones.py
    added a lazy tip

--- checkmilestones.py
+++ checkmilestones.py
@@ -6,8 +6,9 @@
 #   
 # Description: 
 # Python script to alarm you with N beeps and an alert window when any milestone reach the
-# desired percentage at Github. Only for Windows for now. Combine with cron/"Schedule Tasks"
-# to repeat the script as your needs. Be the first to know the big news!
+# desired percentage at Github. Only for Windows for now. Combine with cron/"Schedule Tasks" 
+# or set "while True:" after Main comentary if you are lazy, to repeat the script as your needs. 
+# Be the first to know the big news!
 
 from bs4 import BeautifulSoup
 from time import sleep
